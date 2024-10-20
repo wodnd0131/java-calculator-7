5755a970750b791329a33f6511a2e569e9a7d768
황재웅 <wodnd0131@naver.com>
2024-10-20 22:18:59 +0900

    chore: 예외처리 메세지 문을 정정한다
    - ErrorMessages 클래스 내 미사용 메세지를 삭제한다
    - ErrorMessages 클래스와 README의 명세된 예외 처리 내용을 정정한다.

--- src/main/java/calculator/common/ErrorMessages.java
+++ src/main/java/calculator/common/ErrorMessages.java
@@ -1,12 +1,11 @@
 package calculator.common;
 
 public enum ErrorMessages {
-	INVALID_CUSTOM_SEPARATOR("커스텀 구분자 지정이 올바르지 않습니다"),
-	SEPARATOR_NOT_ALLOWED("허용되지 않은 구분자입니다"),
+	SEPARATOR_NOT_ALLOWED("잘못된 구분자 처리입니다"),
 	CUSTOM_SEPARATOR_PREFIX("커스텀 구분자는 '//'로 시작해야 합니다"),
 	CUSTOM_SEPARATOR_LENGTH("커스텀 구분자는 단일 문자여야 합니다"),
 	CUSTOM_SEPARATOR_NUMERIC("커스텀 구분자에 숫자가 포함될 수 없습니다"),
-	INVALID_INPUT("입력값이 올바르지 않습니다");;
+	;
 
 	private final String message;
 
